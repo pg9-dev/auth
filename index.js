30186e61785c05d5d9407df45fdbca6bd7390eb6
pg9-dev <prabhat.gehlot.737@my.csun.edu>
2020-05-29 18:31:23 -0700

    connects to db and adds a sample model

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express(); 
-const PORT = 3000; 
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 // Importing routes
-const authRoute = require('./routes/auth'); 
+const authRoute = require("./routes/auth");
+
+const PORT = 8000;
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("connected to db!");
+    }
+);
+
+// Middleware
+app.use(express.json());
 
 // Route MiddleWare
-app.use('/api/user', authRoute); 
+app.use("/api/user", authRoute);
 
-app.listen(PORT, () => console.log("Server is running on port: " + PORT)); 
+app.listen(PORT, () => console.log("Server is running on port: " + PORT));
