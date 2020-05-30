6da1ff6df06ed6609bbc17e0cc236c829acd1c0a
pg9-dev <prabhat.gehlot.737@my.csun.edu>
2020-05-30 12:05:48 -0700

    Adds new route for login validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,52 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 const User = require("../model/User");
-const  {registerValidation} = require('../validation');
+const { registerValidation, loginValidation } = require("../validation");
+
 
 router.post("/register", async (req, res) => {
     // Validate data
-    const {error} = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message); 
-    
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     // Checking if the user is already in the databse
-    const emailExist = await User.findOne({email: req.body.email});
+    const emailExist = await User.findOne({ email: req.body.email });
     if (emailExist) return res.status(400).send("Email Already Exists");
+
+    // Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
 
     // Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: savedUser._id});
     } catch (err) {
         res.status(400).send(err);
     }
 });
 
+// Log In
+router.post("/login", async (req, res) => {
+    // Validate data
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Checking if the email exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Login credentials don't exists.");
+
+    // Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send("Login credentials aren't valid.");
+
+    res.send('Login Success');
+
+})
 module.exports = router;
